014e05c5094e126ddedf9c8336f6df6b4dd0da13
josecorvalan37 <jo.corvalanf@duocuc.cl>
2025-04-09 09:31:04 -0400

    clase 2 python codigo
    ssuma resta division multiplicacion division

---
+++ clase 2 python.py
@@ -0,0 +1,12 @@
+V01 = int(input("dime tu numero entero: "))
+V02 = float(input("dime tu numero decimal: "))
+
+# operacion
+
+r01 = V01+ V02
+r02 = V01- V02
+r03 = V01* V02
+r04= V01/ V02
+
+# salida
+print(f"el resultado es: {r01, r02, r03, r04}")
